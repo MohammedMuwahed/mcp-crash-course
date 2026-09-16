39e60f92b454b39e8dc84f45c4c7c483c4108290
Mohammad Muwahed <103749096+MohammedMuwahed@users.noreply.github.com>
2026-09-17 02:19:14 +0300

    Enhance main.py to include HumanMessage import, update stdio_server_params path format, and implement asynchronous session initialization with agent invocation for mathematical queries.

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+from langchain_core.messages import HumanMessage
 from mcp import ClientSession, StdioServerParameters
 from mcp.client.stdio import stdio_client
 
@@ -15,11 +16,20 @@ llm = ChatGoogleGenerativeAI(model="gemini-3.6-flash")
 
 stdio_server_params = StdioServerParameters(
     command="python",
-    args=["C:\\Users\\moham\\Documents\\GitHub\\mcp-crash-course\\server\\math_server.py"],
+    args=["C:/Users/moham/Documents/GitHub/mcp-crash-course/server/math_server.py"],
 )
 
 async def main():
-    print("Hello from mcp-crash-course")
+    async with stdio_client(stdio_server_params) as (read,write):
+        async with ClientSession(read_stream=read,write_stream=write) as session:
+            await session.initialize()
+            print("Session initialized")
+            tools = await load_mcp_tools(session)
+            
+            agent = create_agent(llm,tools)
+
+            result = await agent.ainvoke({"messages": [HumanMessage(content = "what is 54 + 2 * 3")]})
+            print(result["messages"][-1].content)
 
 if __name__ == "__main__":
     asyncio.run(main())
